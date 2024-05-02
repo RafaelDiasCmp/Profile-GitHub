56b01db61ea8bc6c84f77df4be51d43307fa7a65
Rafael Dias Campos <raffaeldiascampos@gmail.com>
2024-05-02 15:30:53 -0300

    closes #3 Erro caso o usuário não exista

--- JS/script.js
+++ JS/script.js
@@ -3,19 +3,39 @@ function search() {
 
     var username = document.getElementById("inputUserName").value;
 
-    var url = ` https://api.github.com/users/${username}`;
+    var url = `https://api.github.com/users/${username}`;
 
     $.getJSON(url, (user) => {
+        showUserData(user);
+        clearError();
 
-        document.getElementById("name").innerHTML =         user.name;
-        document.getElementById("html_url").innerHTML =     user.html_url;
-        document.getElementById("company").innerHTML =      user.company;
-
-        document.getElementById("avatar_url").innerHTML = `
-
-        <img src="${user.avatar_url}" id="avatar_url" width="220" height="220"
-        class="shadow rounded"></img>`
+    }).fail(() => {
+        showUserData({});
+        showError("Usuário não encontrado!");
 
     });
 
+}
+
+function showError(msg) {
+    document.getElementById("error").innerHTML = `<div class='alert alert-danger mt-2' role='alert'>${msg}</div>`
+}
+
+function clearError() {
+    document.getElementById("error").innerHTML = "";
+}
+
+
+function showUserData(user) {
+    document.getElementById("name").innerHTML = user.name || "";
+    document.getElementById("html_url").innerHTML = user.html_url || "";
+    document.getElementById("company").innerHTML = user.company || "";
+
+    document.getElementById("avatar_url").innerHTML = user.avatar_url ?
+        `
+
+                                                       <img src="${user.avatar_url}" id="avatar_url" width="220" height="220"
+                                                       class="shadow rounded"></img>
+                                                       ` :
+        "";
 }
